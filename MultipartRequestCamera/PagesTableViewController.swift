ba61257bce408047806ac16b39462f573e226467
Nikita <nordzooldev@gmail.com>
2024-10-04 19:40:03 +0300

    Implement DataSource for PagesTableViewController

--- MultipartRequestCamera/PagesTableViewController.swift
+++ MultipartRequestCamera/PagesTableViewController.swift
@@ -13,9 +13,23 @@ class PagesTableViewController : UIViewController {
         let table = UITableView()
         table.delaysContentTouches = false
         table.backgroundColor = .blue
-        
+        table.dataSource = self
         return table
     }()
+    
+    //MARK: - Private properties
+    private let viewModel: PagesViewModel
+    
+    //MARK: - Inits
+    init(viewModel: PagesViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     //MARK: - Live cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,5 +50,30 @@ class PagesTableViewController : UIViewController {
     func updateTableViewLayout(with size:CGSize) {
         self.tableView.frame = CGRect.init(origin: .zero, size: size)
     }
+    
+    //Дописать подписку на изменения pageTypes в VM.
 }
 
+extension PagesTableViewController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewModel.pagesTypeSubject.value.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.pagesTypeSubject
+            .value[section]
+            .content.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let pageContent = viewModel.pagesTypeSubject
+            .value[indexPath.section]
+            .content[indexPath.row]
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = "id: \(pageContent.id) , name: \(pageContent.name)"
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+}
