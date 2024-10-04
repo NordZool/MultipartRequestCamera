c540cf4370819ea1bed128e837e3f6aeadc35538
Nikita <nordzooldev@gmail.com>
2024-10-04 11:59:41 +0300

    initial commit

---
+++ MultipartRequestCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipartRequestCamera
+//
+//  Created by nikita on 4.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
