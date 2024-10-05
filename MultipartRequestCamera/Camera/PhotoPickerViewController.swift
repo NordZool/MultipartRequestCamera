5a4628d8c3b1c8704714b97ba637ed2ca95a16c8
Nikita <nordzooldev@gmail.com>
2024-10-05 22:08:25 +0300

    setup PhotoPickerViewController

--- MultipartRequestCamera/Camera/PhotoPickerViewController.swift
+++ MultipartRequestCamera/Camera/PhotoPickerViewController.swift
@@ -5,4 +5,17 @@
 //  Created by nikita on 5.10.24.
 //
 
-import Foundation
+import UIKit
+
+class PhotoPickerViewController : UIImagePickerController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let types = UIImagePickerController.availableMediaTypes(for: .camera) ?? []
+        mediaTypes = types
+        sourceType = .camera
+        cameraFlashMode = .off
+        cameraCaptureMode = .photo
+        cameraDevice = .rear
+        allowsEditing = false
+    }
+}
